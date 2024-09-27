19a47ff20edbcd837b4e3aaa357ca651879eb36e
Maikakajal <119149125+Maikakajal@users.noreply.github.com>
2024-09-27 08:44:35 +0530

    Create Maximum index.cpp

---
+++ Maximum index.cpp
@@ -0,0 +1,9 @@
+#Given an array arr of positive integers. The task is to return the maximum of j - i subjected to the constraint of arr[i] < arr[j] and i < j.
+class Solution {
+  public:
+    // arr[]: input array
+    // Function to find the maximum index difference.
+    int maxIndexDiff(vector<int>& arr) {
+        // Your code here
+    }
+};
