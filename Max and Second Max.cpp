da14a8f49dab9894cab24c96b4b821539b3e38f2
Maikakajal <119149125+Maikakajal@users.noreply.github.com>
2024-09-27 08:45:37 +0530

    Create Max and Second Max.cpp

---
+++ Max and Second Max.cpp
@@ -0,0 +1,20 @@
+#Given an array arr[] of size N of positive integers which may have duplicates. The task is to find the maximum and second maximum from the array, and both of them should be different from each other, so If no second max exists, then the second max will be -1.
+class Solution{
+  public:
+    /* Function to find largest and second largest element
+    *sizeOfArray : number of elements in the array
+    * arr = input array
+    */
+    vector<int> largestAndSecondLargest(int sizeOfArray, int arr[]){
+        int max = INT_MIN, max2= INT_MIN;
+        
+        /*********************************
+         * Your code here
+         * This function should return a
+         * vector with first element as
+         * max and second element as 
+         * second max
+         * *******************************/
+         
+    }
+};
